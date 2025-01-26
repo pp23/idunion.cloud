512a241916a73bf527ed3216b8645cdfa9adf997
Paul Peschel <paul.peschel@posteo.de>
2025-01-26 21:35:37 +0100

    Add image pull secrets for idunion ghcr.io images
    Reads a passed in dockerconfig.json file and creates a dockerconfigjson
    kubernetes secret out of it.

--- main.tf
+++ main.tf
@@ -1,19 +1,28 @@
-data "kustomization_build" "cloud" {
-  path  = "overlays/${var.env}"
+data "kustomization_overlay" "cloud" {
+  resources = ["overlays/${var.env}"]
   kustomize_options {
     enable_alpha_plugins = true
-    enable_helm = true
-    helm_path = var.helm_path
+    enable_helm          = true
+    helm_path            = var.helm_path
+  }
+
+  secret_generator {
+    name      = "container-registry-credentials"
+    namespace = "idunion"
+    type      = "kubernetes.io/dockerconfigjson"
+    literals = [
+      ".dockerconfigjson=${file(var.idunion_dockerconfig_path)}",
+    ]
   }
 }
 
-resource "kustomization_resource" "cloud-common" {
-  for_each  = data.kustomization_build.cloud.ids_prio[0]
-  manifest  = data.kustomization_build.cloud.manifests[each.value]
+resource "kustomization_resource" "cloud_common" {
+  for_each = data.kustomization_overlay.cloud.ids_prio[0]
+  manifest = data.kustomization_overlay.cloud.manifests[each.value]
 }
 
 resource "kustomization_resource" "cloud" {
-  for_each  = data.kustomization_build.cloud.ids_prio[1]
-  manifest  = data.kustomization_build.cloud.manifests[each.value]
-  depends_on  = [kustomization_resource.cloud-common]
+  for_each   = data.kustomization_overlay.cloud.ids_prio[1]
+  manifest   = data.kustomization_overlay.cloud.manifests[each.value]
+  depends_on = [kustomization_resource.cloud_common]
 }
--- vars.tf
+++ vars.tf
@@ -1,14 +1,19 @@
 variable "kubeconfig" {
-  type = string
+  type        = string
   description = "Path to the kube config file"
 }
 
 variable "env" {
-  type = string
+  type        = string
   description = "Deployment environment which are the overlays like 'development'"
 }
 
 variable "helm_path" {
-  type = string
+  type        = string
   description = "Path to the Helm binary"
+}
+
+variable "idunion_dockerconfig_path" {
+  type        = string
+  description = "Path to the dockerconfig to be used to pull images from the container registry"
 }
